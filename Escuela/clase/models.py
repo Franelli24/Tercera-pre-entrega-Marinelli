cefb4ce46df0394ede75a3fc9c2f50dc4b00e127
Franco Marinelli <franco.marinelli@gmail.com>
2024-05-12 11:15:08 -0300

    feat(Clase): creación de modelo

--- Escuela/clase/models.py
+++ Escuela/clase/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Curso(models.Model):
+    nombre = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.nombre
+    
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.nombre
+    
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.nombre
+    
+class Comision(models.Model):
+    nombre = models.PositiveIntegerField(unique=True)
+    curso = models.ForeignKey(Curso, on_delete=models.SET_NULL, null=True, blank=True)
+    profesor = models.ForeignKey(Profesor, on_delete=models.SET_NULL, null=True, blank=True)
+    estudiante = models.ManyToManyField(Estudiante)
