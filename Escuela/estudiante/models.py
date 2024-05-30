c63676b7e91ac27cb67ebd12a89d79e67c2f8e38
Franco Marinelli <franco.marinelli@gmail.com>
2024-05-30 14:10:53 -0300

    f

--- Escuela/estudiante/models.py
+++ Escuela/estudiante/models.py
@@ -1,37 +1,16 @@
-from django.conf import settings
-from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
 # Create your models here.
 
-class Admin(AbstractUser):
-        groups = models.ManyToManyField(
-        Group,
-        related_name='admin_set',  # Cambio aquí
-        blank=True,
-        help_text='The groups this user belongs to.',
-        verbose_name='groups'
-    )
-
-        user_permissions = models.ManyToManyField(
-        Permission,
-        related_name='admin_user_set',  # Cambio aquí
-        blank=True,
-        help_text='Specific permissions for this user.',
-        verbose_name='user permissions'
-    )
-
-        def __str__(self) -> str:
-            return f"{self.first_name} {self.last_name}" if self.first_name and self.last_name else self.username
-
 class Estudiante(models.Model):
-    usuario = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="estudiante")
+    usuario = models.OneToOneField("clase.Estudiante", on_delete=models.CASCADE, related_name="estudiante")
     celular = models.CharField(max_length=100)
     avatar = models.ImageField(upload_to="avatares", null=True, blank=True)
 
     def __str__(self) -> str:
-        return f"{self.usuario.username}"
+        return self.usuario.nombre
 
 class Clase(models.Model):
     estudiante = models.ForeignKey(Estudiante, on_delete=models.SET_NULL, null=True, blank=True)
